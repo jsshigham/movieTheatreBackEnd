6252a60dcb5178a7134fdc93fa786ba191050d54
Jonathon Higham <109483370+jsshigham@users.noreply.github.com>
2023-09-12 23:23:22 -0500

    no extra files

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv')
 dotenv.config()
 const mongoose = require('mongoose');
-const movieRoute = require('./routes/movieRoute.js');
+// const movieRoute = require('./routes/movieRoute.js');
 const cors = require('cors');
 
 const app = express();
@@ -19,7 +19,118 @@ app.use(cors());
 //      allowedHeaders: ["Content-Type"]
 // }))
 
-app.use("/movies", movieRoute)
+// app.use("/movies", movieRoute)
+
+app.post("/api/movies", async (request, response) => {
+    try {
+      if (
+        !request.body.title ||
+        !request.body.genre ||
+        !request.body.releaseYear ||
+        !request.body.showTime1
+      ) {
+        return response.status(400).send({
+          message: "send all required fields, title, relaseYear, genre, showTime1",
+        });
+      }
+     
+      const newMovie = {
+        title: request.body.title,
+        genre: request.body.genre,
+        releaseYear: request.body.releaseYear,
+        showTime1: request.body.showTime1,
+        showTime2: request.body.showTime2,
+        showTime3: request.body.showTime3,
+        showTime4: request.body.showTime4,
+        showTime5: request.body.showTime5,
+        showTime6: request.body.showTime6,
+      };
+  
+      const movie = await Movie.create(newMovie);
+  
+      response.status(201).send(movie);
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+  
+  app.get("/api/movies", async (request, response) => {
+    try {
+      const movies = await Movie.find({});
+  
+      return response.status(200).send({
+        count: movies.length,
+        data: movies,
+      });
+    } catch (error) {
+      console.log(error);
+      response.status(500).json({ message: error.message });
+    }
+  });
+  
+  app.get("/api/movies/:id", async (request, response) => {
+    try {
+      const { id } = request.params;
+      const movie = await Movie.findById(id);
+  
+      return response.status(200).json({
+        movie,
+      });
+    } catch (error) {
+      console.log(error);
+      response.status(500).send({ message: error.message });
+    }
+  });
+  
+  app.put("/api/movies/:id", async (request, response) => {
+    try {
+      if (
+        !request.body.title ||
+        !request.body.genre ||
+        !request.body.releaseYear ||
+        !request.body.showTime1
+      ) {
+        return response.status(400).send({
+          message: "send all required fields, title, relaseYear, genre, showTime1",
+        });
+      }
+  
+      const { id } = request.params;
+  
+      const result = await Movie.findByIdAndUpdate(id, request.body);
+  
+      if (!result) {
+        return response.status(404).send({
+          message: "movie not found",
+        });
+      }
+  
+      response.status(200).send({ message: "movie updated" });
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+  
+  app.delete("/api/movies/:id", async (request, response) => {
+    try {
+      const { id } = request.params;
+  
+      const result = await Movie.findByIdAndDelete(id);
+  
+      if (!result) {
+        return response.status(404).send({
+          message: "movie not found",
+        });
+      }
+  
+      return response.status(202).send({ message: "movie deleted" });
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
 
 
 const PORT = process.env.PORT
